a44ca6ca69cb4c360df81d626c313bf3f28867dd
chervonoderevnyk <igorchornyj8@gmail.com>
2025-03-20 16:57:51 +0200

    Add scss Part 1

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { UsersComponent } from "./users/users.component";
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @Component({
   selector: 'app-root',
-  imports: [UsersComponent],
+  imports: [UsersComponent, PostsComponent, CommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
